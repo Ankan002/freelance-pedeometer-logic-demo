20ad0335904610d9611382df0dc8a516f17533bc
ankanbhattacharya <ankanbhattachatya89@gmail.com>
2022-08-01 22:24:53 +0530

    build: :rocket: v1.0.0beta08 complete

--- App.tsx
+++ App.tsx
@@ -1,10 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View, Platform } from 'react-native';
+import { usePedometer } from '@use-expo/sensors';
+import { Pedometer } from 'expo-sensors';
+import { useEffect, useState } from 'react';
+import { check, request, PERMISSIONS, RESULTS } from "react-native-permissions";
+
 
 export default function App() {
+
+  const [data, isAvailable] = usePedometer();
+  const [steps, setSteps] = useState<number>(0);
+
+  useEffect(() => {
+    if(isAvailable){
+      subscribe();
+    }
+  }, [isAvailable]);
+
+  const subscribe = async() => {
+    if(Platform.Version >= 29){
+      const permissionStatus = await check(PERMISSIONS.ANDROID.ACTIVITY_RECOGNITION);
+
+      if(permissionStatus !== RESULTS.GRANTED){
+        const Result = await request(PERMISSIONS.ANDROID.ACTIVITY_RECOGNITION);
+
+        if(Result !== RESULTS.GRANTED) {
+          Alert.alert("Destruction....");
+          return;
+        }
+      }
+    }
+    else{
+      const permissionStatus = (await Pedometer.getPermissionsAsync()).granted;
+
+      if(!permissionStatus){
+        await Pedometer.requestPermissionsAsync();
+      }
+    }
+
+    Pedometer.watchStepCount(res => {
+      setSteps(res.steps);
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Your Steps</Text>
+      <Text>{steps}</Text>
       <StatusBar style="auto" />
     </View>
   );
